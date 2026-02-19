fc03c05bd7f11df6380d86f6290621fc7d26cf6e
chintan <chintan@gmail.com>
2026-02-19 09:07:12 +0530

    basic

---
+++ 001_basic/Demo.py
@@ -0,0 +1,17 @@
+# #include<stdio.h>
+# #include<conio.h>
+# void main()
+# {
+#     printf("Hello world");
+# }
+
+
+# class Demo{
+#     public static void main(String args[])
+#     {
+#         System.out.println("Hello world");
+#     }
+# }
+
+
+print("Hello world")
