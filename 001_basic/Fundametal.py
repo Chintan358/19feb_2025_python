f85f25e1e4e6d46a6fc4d9049e267b412f40d105
chintan <chintan@gmail.com>
2026-02-21 09:14:34 +0530

    datatypes

---
+++ 001_basic/Fundametal.py
@@ -0,0 +1,25 @@
+# print("Hello python")
+# print('Hello   world')
+# print("""Hello python
+#        fffd
+#       fdfdfd
+#       fdf
+#       """)
+
+# print("Python","Java","Php","Android",sep=" -|- ")
+
+# print("Hello",end=" - ")
+# print("python")
+
+
+# Hello \" Python
+
+# print('Hello " Python')
+
+# print("Hello\b \\\" C ")
+
+
+# a = input("Enter value of a : ")
+# print(a)
+
+print("Hello")
