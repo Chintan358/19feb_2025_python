d30ead1c330078f223f3fbb95d7810fc875c0e7d
chintan <chintan@gmail.com>
2026-06-09 09:10:59 +0530

    all

--- 015_ajax/myapp/urls.py
+++ 015_ajax/myapp/urls.py
@@ -3,5 +3,10 @@ from myapp.views import *
 
 urlpatterns = [
     path("",index,name="index"),
-    path("test",test,name="test")
+    path("test",test,name="test"),
+    path("search",search,name="search"),
+    
+    path("countries",get_countries,name="countries"),
+    path("states",get_states,name="states"),
+    path("cities",get_cities,name="cities")
 ]
--- 015_ajax/myapp/views.py
+++ 015_ajax/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+from myapp.models import *
 # Create your views here.
 def index(request):
     return render(request, "index.html")
@@ -7,3 +8,33 @@ def index(request):
 def test(request):
     q = request.GET['q']
     return HttpResponse(f"Hello, {q}")
+
+def search(request):
+    q = request.GET['q']
+    # result = ""
+    # if q=='electric':
+    #     result = "<ul><li>Fan</li><li>TV</li><li>Mobile</li></ul>"
+    # elif q=='cloths':
+    #     result = "<ul><li>Shirt</li><li>Tshirt</li><li>Cap</li></ul>"
+    products = Product.objects.filter(name__startswith=q)
+    
+    result = "<ul>"
+    for product in products:
+        result+=f"<li>{product.name}</li>"
+    result+="</ul>"
+    print(result)
+    return HttpResponse(result)
+
+def get_countries(request):
+    countries = Country.objects.all()
+    return JsonResponse({"data":list(countries.values())})
+
+def get_states(request):
+    cid = request.GET['cid']
+    states = State.objects.filter(country_id=cid)
+    return JsonResponse({"data":list(states.values())})
+
+def get_cities(request):
+    sid = request.GET['sid']
+    cities = City.objects.filter(state_id=sid)
+    return JsonResponse({"data":list(cities.values())})
